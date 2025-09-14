01ddaa022fb123932fe094a1aefc846da29582bd
atharv <atharv.shinde24@pccoepune.org>
2025-09-14 20:12:36 +0530

    fucked vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
+  
   // IMPORTANT: The name here MUST match your GitHub repository name exactly.
-  base: '/invictus-gym-app/', 
+  base: '/', 
 })
